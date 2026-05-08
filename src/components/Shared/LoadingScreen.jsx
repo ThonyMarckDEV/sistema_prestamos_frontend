232bf953c365004d27e8a98b5ff12a7834ee647c
ThonyMarckDEV <thonymarckdev@gmail.com>
2026-05-07 23:47:52 -0500

    ADDED NEW ANIMATION

--- src/components/Shared/LoadingScreen.jsx
+++ src/components/Shared/LoadingScreen.jsx
@@ -1,19 +1,18 @@
 import React from 'react';
 
-import {FadeLoader} from 'react-spinners';
+import { DotLottieReact } from '@lottiefiles/dotlottie-react';
 
 const LoadingScreen = () => {
   return (
     <div className="fixed inset-0 bg-white/80 backdrop-blur-sm flex flex-col items-center justify-center z-50 transition-all duration-300">
       
 
-      <FadeLoader 
-        color="#000000" 
-        size={50}
-        speedMultiplier={1.5}
+      <DotLottieReact
+        src="https://lottie.host/9f133909-f84b-4318-8a2e-848ca7651cac/149hwE5G3g.lottie"
+        loop
+        autoplay
       />
       
-
       <p className="mt-6 text-gray-500 text-sm font-medium tracking-widest uppercase animate-pulse">
         Cargando ...
       </p>
